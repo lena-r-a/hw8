61cad355a6e2f3a30918353098764148df328755
lena-r-a <75614947+lena-r-a@users.noreply.github.com>
2021-01-28 10:01:28 +0300

    1

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
-let large_log=document.getElementById('logo');
-let small_log=document.getElementById('logo__inner')
-
-debugger;
-// large_log.style.backgroundColor='rgb(24,1,10)';
-
-function changeBgColor (color) {
-    large_log.style.backgroundColor=color;
-   
-    }
+let large_log = document.getElementById('logo');
+let small_log = document.getElementById('logo__inner')
 
 
-function randomColor () {
-    r=Math.floor(Math.random()*148)+28;
-    g=Math.floor(Math.random()*148)+28;
-    b=Math.floor(Math.random()*148)+28;
-    
-    return 'rgb('+r+','+g+','+b+')';
+function changeBgColor(color) {
+    large_log.style.backgroundColor = color;
+
 }
 
- var interval = setInterval(function(){changeBgColor(randomColor());},5000);
+
+function randomColor() {
+    r = Math.floor(Math.random() * 148) + 28;
+    g = Math.floor(Math.random() * 148) + 28;
+    b = Math.floor(Math.random() * 148) + 28;
+
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+}
+
+var interval = setInterval(function () { changeBgColor(randomColor()); }, 5000);
 
 
-small_log.addEventListener('mouseover',function(){
-       
+small_log.addEventListener('mouseover', function () {
+
     changeBgColor(randomColor());
     large_log.classList.remove('trans');
+    
     clearInterval(interval);
-    interval=null;})
+    interval = null;
+})
 
-    small_log.addEventListener('mouseout',function() {
-        large_log.classList.add('trans');
-        if (interval==null){
-        interval = setInterval(function(){changeBgColor(randomColor());},5000);};
-    })
+small_log.addEventListener('mouseout', function () {
+    large_log.classList.add('trans');
+    if (interval == null) {
+        interval = setInterval(function () { changeBgColor(randomColor()); }, 5000);
+    };
+})
 
 
 
